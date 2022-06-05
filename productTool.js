f97bc1231fde77cdfb4953a0e8b9979d17563739
Pedro Morais <pedro.s.morais@gmail.com>
2022-06-05 16:01:31 -0300

    criating main.js

--- productTool.js
+++ productTool.js
@@ -1,3 +1,5 @@
+console.log('CHAMOU')
+
 var editorTemplate = '<button id="addProduct" class="button">Add Product</button>';
 
 var productItemsTemplate = _.template(`
@@ -225,10 +227,6 @@ unlayer.registerTool({
   },
   transformer: (values, source) => {
     const { name, value, data } = source;
-
-    console.log('name: ', name)
-    console.log('value: ', value)
-    console.log('values: ', values)
 
     // Transform the values here
     // We will update selected values in property editor here
