0879381edcfa83967da8d78b6442501c900669a3
Pedro S Morais <pedro.smorais@localiza.com>
2022-06-06 16:10:25 -0300

    renderizing produts

--- productTool.js
+++ productTool.js
@@ -47,14 +47,26 @@ const modalTemplate = function (data) {
 
 const productItemsTemplate = _.template(`
 <% _.forEach(products, function(item) { %>
-  <div class="product-item" id="product-item" data-uuid='<%= item.id %>' data-title="<%= item.title %>" data-price="<%= item.price %>" data-image="<%= item.image %>" data-description="<%= item.description %>" >
-  <img src="<%= item.image %>" style="max-height: 300px;min-height: 300px;width: 100%;" />
-    <h4 style="margin: 0.5rem 0; text-align: left;"><%= item.title %></h4>
-    <h4 style="margin: 0.5rem 0; text-align: left;">$<%= item.price %></h4>
-    <p style="text-align: left;"><%= item.description %></p>
-  </div>
+<div class="product-item" id="product-item" data-uuid='<%= item.id %>' data-title="<%= item.title %>" data-price="<%= item.price %>" data-image="<%= item.image %>" data-description="<%= item.description %>" >
+<img src="<%= item.image %>" style="max-height: 300px;min-height: 300px;width: 100%;" />
+  <h4 style="margin: 0.5rem 0; text-align: left;"><%= item.title %></h4>
+  <h4 style="margin: 0.5rem 0; text-align: left;">$<%= item.price %></h4>
+  <p style="text-align: left;"><%= item.description %></p>
+</div>
 <% }); %>
 `);
+
+const editorTemplate = `<button id="addProduct" class="button">Add Product</button>`;
+
+const showModal = function () {
+  const modal = document.getElementById('product_library_modal');
+  modal.classList.add('show');
+};
+
+const hideModal = function () {
+  const modal = document.getElementById('product_library_modal');
+  modal.classList.remove('show');
+};
 
 unlayer.registerTool({
   name: 'product_tool',
@@ -176,5 +188,169 @@ unlayer.registerTool({
         return toolTemplate(values);
       },
     },
+    head: {
+      // As we need custom styling in export as well that's why we put those styles here
+      css: function (values) {
+        return `      
+        .product-card {
+          position: relative;
+          display: -webkit-box;
+          display: -ms-flexbox;
+          display: flex;
+          -webkit-box-orient: vertical;
+          -webkit-box-direction: normal;
+          -ms-flex-direction: column;
+          flex-direction: column;
+          min-width: 0;
+          word-wrap: break-word;
+          background-color: #fff;
+          background-clip: border-box;
+          border: 1px solid rgba(0,0,0,.125);
+          border-radius: .25rem;
+          margin: auto;
+          text-align: center;
+        }
+
+        .product-card-body {
+          padding: 0 1rem 1rem;
+          text-align: left;
+        }
+
+        .product-card-body h3 {
+          margin: 0.7rem 0;
+        }
+
+        .product-card img {
+          width: 100%;
+          object-fit: contain;
+          border-top-left-radius: 0.25rem;
+          border-top-right-radius: 0.25rem;
+        }
+
+        .product-card .product-footer {
+          display: flex;
+          border-bottom-left-radius: 0.25rem;
+          border-bottom-right-radius: 0.25rem;
+          border-top: 1px solid rgba(0,0,0,.125);
+          align-items: center;
+          font-weight: bold;
+        }
+
+        .product-card .product-footer > div, .product-card .product-footer > .button{
+          width: 50%;
+        }
+
+        .product-card .product-footer > div {
+          border-bottom-left-radius: 0.25rem;
+        }
+
+        .product-card .product-footer > .button {
+          border-bottom-right-radius: 0.25rem;
+        }
+
+        .button {
+          display: inline-block;
+          font-weight: 400;
+          color: #ffffff;
+          text-align: center;
+          vertical-align: middle;
+          background-color: transparent;
+          border: 1px solid transparent;
+          border-radius: 0.25rem;
+          padding: .75rem;
+          font-size: 1rem;
+          line-height: 1.5;
+          transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+          background-color: rgb(0, 123, 255);
+          cursor: pointer;
+        }
+
+        .m-0 {
+          margin: 0px;
+        }
+
+        .no-underline {
+          text-decoration: none;
+        }
+
+        .no-border-radius {
+          border-radius: 0px;
+        }
+        `;
+      },
+      js: function (values) { },
+    },
   },
+});
+
+
+
+unlayer.registerPropertyEditor({
+  name: 'product_library',
+  layout: 'bottom',
+  Widget: unlayer.createWidget({
+    render(value, updateValue, data) {
+      return editorTemplate;
+    },
+    mount(node, value, updateValue, data) {
+      var addButton = node.querySelector('#addProduct');
+      addButton.onclick = function () {
+        showModal();
+        setTimeout(() => {
+          // We are using event bubling to capture clicked item instead of registering click event on all product items.
+          var selectButton = document.querySelector('.products-list');
+          if (!selectButton) return;
+          selectButton.onclick = function (e) {
+            if (e.target.id === 'product-item') {
+              // If user clicks on product item
+              // Find selected item from products list
+              const selectedProduct = data.products.find(
+                (item) => item.id === parseInt(e.target.dataset.uuid)
+              );
+              updateValue({ selected: selectedProduct });
+            } else {
+              // If user click on child of product item (e.g. title, price, image or desctiption)
+              const parent = e.target.parentElement;
+              if (parent && parent.id !== 'product-item') return;
+              const selectedProduct = data.products.find(
+                (item) => item.id === parseInt(parent.dataset.uuid)
+              );
+              updateValue({ selected: selectedProduct });
+            }
+            hideModal();
+            // This is a hack to close property editor right bar on selecting an item from products list.
+            var outerBody = document.querySelector('#u_body');
+            outerBody.click();
+          };
+          /* Register event listeners for search */
+          var searchBar = document.querySelector('#search-bar');
+          var searchButton = document.querySelector('#search-btn');
+          searchButton.onclick = function (e) {
+            const list = document.querySelector(
+              '#product_library_modal .products-list'
+            );
+            let filteredItem;
+            let productsListHtml;
+            if (list && data && data.products) {
+              if (searchBar.value === '') {
+                productsListHtml = productItemsTemplate({
+                  products: data.products,
+                });
+              } else {
+                filteredItem = data.products.filter((item) =>
+                  item.title
+                    .toLowerCase()
+                    .includes(searchBar.value.toLowerCase())
+                );
+                productsListHtml = productItemsTemplate({
+                  products: filteredItem,
+                });
+              }
+              list.innerHTML = productsListHtml;
+            }
+          };
+        }, 200);
+      };
+    },
+  }),
 });
